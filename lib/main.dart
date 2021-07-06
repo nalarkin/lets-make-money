4ae9ee588750e7139c54d10f9e5374c90681f523
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-06 15:06:42 -0400

    codecov

--- lib/main.dart
+++ lib/main.dart
@@ -65,3 +65,29 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
