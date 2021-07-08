c1ce87bd182f724f4e973a7ae5d010f1a7589869
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-07-08 15:28:38 -0400

    commit after failed atempt of widget testing profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,16 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:lets_talk_money/utils/widgets.dart';
 import 'package:provider/provider.dart';
 
-class Profile extends StatefulWidget {
+class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
   static final String routeName = '/profile';
 
-  @override
-  _ProfileState createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  @override
   Widget build(BuildContext context) {
     User? currUser = Provider.of<User?>(context);
     return Scaffold(
@@ -62,3 +56,64 @@ class _ProfileState extends State<Profile> {
     );
   }
 }
+
+
+// class Profile extends StatefulWidget {
+//   const Profile({Key? key}) : super(key: key);
+//   static final String routeName = '/profile';
+
+//   @override
+//   _ProfileState createState() => _ProfileState();
+// }
+
+// class _ProfileState extends State<Profile> {
+//   @override
+//   Widget build(BuildContext context) {
+//     User? currUser = Provider.of<User?>(context);
+//     return Scaffold(
+//       appBar: customAppBar(context, "Profile"),
+//       drawer: customDrawer(context),
+//       body: Column(
+//         children: [
+//           // Padding(
+//           //   padding: EdgeInsets.fromLTRB(10, 60, 10, 40),
+//           // child:
+//           Card(
+//             margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+//             child: Stack(
+//               alignment: Alignment.center,
+//               children: [
+//                 Container(
+//                   // color: Colors.blue,
+//                   child: ListTile(
+//                     // minVerticalPadding: 30,
+//                     leading: Icon(
+//                       Icons.face,
+//                       size: 60,
+//                     ),
+//                   ),
+//                 ),
+//                 // Container(
+//                 // color: Colors.red,
+//                 // child:
+//                 Container(
+//                   alignment: Alignment.center,
+//                   margin: EdgeInsets.fromLTRB(10, 30, 10, 30),
+//                   child: Text("${currUser?.displayName}"),
+//                 )
+
+//                 //   ),
+//                 //   Text("${currUser?.displayName}"),
+//               ],
+//             ),
+//             //     ),
+//             //     ),
+//             //   ],
+//             // ),
+//           ),
+//           // )
+//         ],
+//       ),
+//     );
+//   }
+// }
--- test/screens/profile_test.dart
+++ test/screens/profile_test.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -43,7 +46,48 @@ import 'package:provider/provider.dart';
 //             Profile.routeName: (context) => Profile(),
 //           }));
 // }
+// Widget testAuth() {
 
+// }
+
+// class testAuth extends StatelessWidget {
+//   const testAuth({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     User? currUser = Provider.of<User?>(context);
+//     print("CURRENT MOCK USER: $currUser");
+//     return Text(currUser?.displayName ?? '');
+//   }
+// }
+
+// class CreateProfilePage extends StatelessWidget {
+//   const CreateProfilePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     User? currUser = Provider.of<User?>(context);
+//     print("CURRENT MOCK USER: $currUser");
+//     return Profile();
+//   }
+// }
+
+// class ProfileTester {
+//   ProfileTester({required this.firebase});
+
+//   Future setUp() async {
+//     WidgetsFlutterBinding.ensureInitialized();
+//     await Firebase.initializeApp();
+//   }
+
+//   Widget launch() {
+//     return
+//   }
+
+//   FirebaseFirestore firebase;
+// }
+
+const MSG_COLLECTION = 'messages';
 void main() {
   group("Testing Profile Page", () {
     // testWidgets("Testing Initial Load", (tester) async {
@@ -51,19 +95,46 @@ void main() {
     //   expect(find.text("Profile"), findsOneWidget);
     // });
 
-    AuthService _auth = AuthService();
+    // fire_auth.authStateChanges();
+    // fire_auth.signInAnonymously()
+    // print(fire_auth.currentUser)
+    // print("USER IN MAIN ${fire_auth.currentUser}");
+
+    // AuthService _auth = AuthService();
+
     testWidgets("Testing Initial Load", (tester) async {
-      FirebaseApp curr = await Firebase.initializeApp();
+      final firestore = FakeFirebaseFirestore();
+
+      // await firestore.collection(path)
+      final user = MockUser(
+        isAnonymous: false,
+        uid: "SOME_LoNG_ID_69",
+        email: "abc@gmail.com",
+        displayName: "MickeyMouse",
+      );
+      // print("USER IN MAIN $user");
+      final fire_auth = MockFirebaseAuth(mockUser: user, signedIn: true);
+      // WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      print("initialized app");
       await tester.pumpWidget(MultiProvider(
-          providers: [
-            StreamProvider<User?>.value(
-              value: _auth.currentUser,
-              initialData: null,
-            ),
-          ],
-          child: Builder(
-            builder: (_) => Profile(),
-          )));
+        providers: [
+          StreamProvider<User?>.value(
+            value: fire_auth.authStateChanges(),
+            initialData: null,
+          ),
+        ],
+        // child: MaterialApp(home: testAuth()),
+        child: MaterialApp(home: Profile(firestore)),
+        // child: Builder(
+        //   builder: (context) => ,
+        //   // builder: (_) => Profile(),
+        // )));
+        // expect(find.text("Profile"), findsOneWidget);
+      ));
+      await tester.idle();
+      await tester.pump();
+      expect(find.text("MickeyMouse"), findsOneWidget);
       expect(find.text("Profile"), findsOneWidget);
     });
   });
